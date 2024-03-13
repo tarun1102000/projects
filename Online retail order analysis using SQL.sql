df2b4107f70fba6e9b61d368e913e41260b1b0d2
Tarun Rajpurohit <119515084+tarun1102000@users.noreply.github.com>
2024-03-13 14:08:47 +0530

    Online retail order analysis

--- Online retail order analysis using SQL.sql
+++ Online retail order analysis using SQL.sql
@@ -1,6 +1,6 @@
  use orders;
  
- # 1st Question
+/* Q1: Write a query to display customer's full name with their title (Mr/Ms), both first name and last name are in upper case, customer email id, customer creation date and display customer’s category after applying below categorization rules: i) IF customer creation date Year <2005 Then Category A ii) IF customer creation date Year >=2005 and <2011 Then Category B iii)IF customer creation date Year>= 2011 Then Category C) */
 
 SELECT
     CONCAT(CASE CUSTOMER_GENDER
@@ -19,7 +19,7 @@ FROM ONLINE_CUSTOMER;
 
 -------------------------------------------------------------------------------------------------------------
 
-# 2nd Question
+/* Q2: Write a query to display the following information for the products, which have not been sold: product_id, product_desc, product_quantity_avail, product_price, inventory values (product_quantity_avail*product_price), New_Price after applying discount as per below criteria. Sort the output with respect to the decreasing value of Inventory_Value. i) IF Product Price > 20,000 then apply 20% discount ii) IF Product Price > 10,000 then apply 15% discount iii) IF Product Price =< 10,000 then apply 10% discount */
 
 SELECT product.product_id, 
        product.product_desc, 
@@ -37,7 +37,7 @@ ORDER BY "Inventory_Value" DESC;
 
 -------------------------------------------------------------------------------------------------------------
 
-# 3rd Question
+/* Q3: Write a query to display Product_class_code, Product_class_description, Count of Product type in each productclass, Inventory Value (p.product_quantity_avail*p.product_price). Information should be displayed for only those product_class_code which have more than 1,00,000. Inventory Value. Sort the output with respect to the decreasing value of Inventory_Value. */
 
 SELECT product_class.product_class_code, 
        product_class.product_class_desc, 
@@ -52,7 +52,7 @@ ORDER BY "Inventory_Value" DESC;
 
 -------------------------------------------------------------------------------------------------------------
 
-# 4th Question
+/* Q4: Write a query to display customer_id, full name, customer_email, customer_phone and country of customers who have cancelled all the orders placed by them. */
 
 select c.customer_id as CUSTOMER_ID, CONCAT(c.customer_fname," ",c.customer_lname) as FULL_NAME,
 c.customer_email as CUSTOMER_EMAIL, c.customer_phone as CUSTOMER_PHONE,a.country as COUNTRY
@@ -67,7 +67,7 @@ and c.customer_id = a.customer_id;
 
 -------------------------------------------------------------------------------------------------------------
 
-# 5th Question
+/* Q5: Write a query to display Shipper name, City to which it is catering, num of customer catered by the shipper in the city and number of consignments delivered to that city for Shipper DHL */
 
 SELECT shipper.shipper_name, 
        address.city,
@@ -82,7 +82,7 @@ GROUP BY shipper.shipper_name, address.city;
 
 -------------------------------------------------------------------------------------------------------------
 
-# 6th Question
+/* Q6: Write a query to display product_id, product_desc, product_quantity_avail, quantity sold and show inventory Status of products as below as per below condition: a. For Electronics and Computer categories, if sales till date is Zero then show 'No Sales in past, give discount to reduce inventory', if inventory quantity is less than 10% of quantity sold,show 'Low inventory, need to add inventory', if inventory quantity is less than 50% of quantity sold, show 'Medium inventory, need to add some inventory', if inventory quantity is more or equal to 50% of quantity sold, show 'Sufficient inventory' b. For Mobiles and Watches categories, if sales till date is Zero then show 'No Sales in past, give discount to reduce inventory', if inventory quantity is less than 20% of quantity sold, show 'Low inventory, need to add inventory', if inventory quantity is less than 60% of quantity sold, show 'Medium inventory, need to add some inventory', if inventory quantity is more or equal to 60% of quantity sold, show 'Sufficient inventory' c. Rest of the categories, if sales till date is Zero then show 'No Sales in past, give discount to reduce inventory', if inventory quantity is less than 30% of quantity sold, show 'Low inventory, need to add inventory', if inventory quantity is less than 70% of quantity sold, show 'Medium inventory, need to add some inventory', if inventory quantity is more or equal to 70% of quantity sold, show 'Sufficient inventory' */
 
 SELECT
 pr.PRODUCT_ID,
@@ -133,7 +133,7 @@ ON pr.PRODUCT_CLASS_CODE = pc.PRODUCT_CLASS_CODE;
 
 -------------------------------------------------------------------------------------------------------------
 
-# 7th Question
+/* Q7: Write a query to display order_id and volume of the biggest order (in terms of volume) that can fit in carton id 10 */
 
 WITH carton_volume AS (
 SELECT 
@@ -168,7 +168,7 @@ ORDER BY
 
 -------------------------------------------------------------------------------------------------------------
 
-# 8th Question
+/* Q8: Write a query to display customer id, customer full name, total quantity and total value (quantity*price) shipped where mode of payment is Cash and customer last name starts with 'G' */
 
 SELECT 
   ONLINE_CUSTOMER.CUSTOMER_ID,
@@ -185,7 +185,7 @@ GROUP BY ONLINE_CUSTOMER.CUSTOMER_ID;
 
 -------------------------------------------------------------------------------------------------------------
 
-# 9th Question
+/* Q9: Write a query to display product_id, product_desc and total quantity of products which are sold together with product id 201 and are not shipped to city Bangalore and New Delhi. Display the output in descending order with respect to the tot_qty. */
 
 SELECT
 product.product_id,
@@ -205,7 +205,7 @@ ORDER BY total_quantity DESC;
 
 -------------------------------------------------------------------------------------------------------------
 
-# 10th Question
+/* Q10: Write a query to display the order_id,customer_id and customer fullname, total quantity of products shipped for order ids which are even and shipped to address where pincode is not starting with "5" */
     
 select distinct OH.ORDER_ID, OH.CUSTOMER_ID,
 concat(OnlCust.CUSTOMER_FNAME,' ',OnlCust.CUSTOMER_LNAME) as Customer_Full_Name, 
